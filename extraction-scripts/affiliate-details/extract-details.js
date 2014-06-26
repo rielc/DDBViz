67ab60789cdfe638435914d0947ffb021dd5a86e
Gabriel Credico <me@gabrielcredico.de>
2014-06-27 01:09:51 +0200

    tried different queries for fetching data

--- extraction-scripts/affiliate-details/extract-details.js
+++ extraction-scripts/affiliate-details/extract-details.js
@@ -9,72 +9,70 @@ nextQuery = false;
 fileIndex = 0;
 personIndex = 0;
 version = 'version-4';
-dir = '../../extracted-data/networks-in-time_fct/'+version+'/';
-saveDir = '../../extracted-data/networks-in-time_fct/version-5/';
+file = '../../extracted-data/networks-in-time_fct/version-5/network_40.json';
+saveDir = '../../extracted-data/affiliate-details/version-5/';
 
 
 analyze = function(minOccurrence) {
 
-    time_fcts = {};
 
+    data = require(file);
+    
     var min = minOccurrence;
+    fs.readdirSync(dir+epochFolder).forEach( function(personFile, index) {
 
-    // parse all the files
-    fs.readdirSync( dir ).forEach( function(epochFolder, index) {
-        if (epochFolder != '.DS_Store') {
-            fs.readdirSync(dir+epochFolder).forEach( function(personFile, index) {
-                if (personFile != '.DS_Store') {
-                    data = require(dir+epochFolder+'/'+personFile);
-                    // generate the epoch 
-                    if ( time_fcts[data.time_fct] == undefined ) {
-                        console.log('Added Epoch: ' + data.time_fct);
-                        time_fcts[data.time_fct] = {};
-                        time_fcts[data.time_fct].links = [];
-                        time_fcts[data.time_fct].nodes = [];
+        if (personFile != '.DS_Store') {
+            data = require(dir+epochFolder+'/'+personFile);
+
+            // generate the epoch 
+
+            if ( time_fcts[data.time_fct] == undefined ) {
+                console.log('Added Epoch: ' + data.time_fct);
+                time_fcts[data.time_fct] = {};
+                time_fcts[data.time_fct].links = [];
+                time_fcts[data.time_fct].nodes = [];
+            }
+            
+            if (data.affiliate_fct_occurrence >= minOccurrence) {
+                // new Person 1
+                if( time_fcts[data.time_fct].nodes.map(function(n) { return n.facet_id; }).indexOf(data.affiliate_fct_id) == -1 ) {
+                    time_fcts[data.time_fct].nodes.push(
+                        {
+                            affiliate_fct_id: data.affiliate_fct_id,
+                            affiliate_fct: data.affiliate_fct,
+                            affiliate_fct_occurrence: data.affiliate_fct_occurrence
+                        });
+                    console.log('Added Node: ' + data.affiliate_fct + ' in ' + data.time_fct);
+                }
+
+
+            sourceFacetID = data.affiliates.affiliate_fct_id;
+            data.affiliates.forEach(
+                function(affiliate) {
+                    // new Person 2
+                    if (affiliate.affiliate_fct_occurrence >= min) {
+                        //console.log(min);
+                        if ( time_fcts[data.time_fct].nodes.map(function(n) { return n.affiliate_fct_id; }).indexOf(affiliate.affiliate_fct_id) == -1) {
+                            time_fcts[data.time_fct].nodes.push(affiliate);
+                            //console.log('Added Node: ' + affiliate.value + ' in ' + data.time_fct);
+                        }
+                        var link = {
+                            target_affiliate_fct_id : data.affiliate_fct_id, 
+                            source_affiliate_fct_id : affiliate.affiliate_fct_id, 
+                            value : affiliate.occurrence
+                        };
+                        time_fcts[data.time_fct].links.push(link);
+                        //console.log(time_fcts[data.time_fct]);
+                        //console.log('Added Link: ' + time_fcts[data.time_fct].nodes[link.source].affiliate_fct + ' > ' + time_fcts[data.time_fct].nodes[link.target].affiliate_fct);
                     }
-                    
-                    if (data.affiliate_fct_occurrence >= minOccurrence) {
-                        // new Person 1
-                        if( time_fcts[data.time_fct].nodes.map(function(n) { return n.facet_id; }).indexOf(data.affiliate_fct_id) == -1 ) {
-                            time_fcts[data.time_fct].nodes.push(
-                                {
-                                    affiliate_fct_id: data.affiliate_fct_id,
-                                    affiliate_fct: data.affiliate_fct,
-                                    affiliate_fct_occurrence: data.affiliate_fct_occurrence
-                                });
-                            console.log('Added Node: ' + data.affiliate_fct + ' in ' + data.time_fct);
-                        }
 
-
-                    sourceFacetID = data.affiliates.affiliate_fct_id;
-                    data.affiliates.forEach(
-                        function(affiliate) {
-                            // new Person 2
-                            if (affiliate.affiliate_fct_occurrence >= min) {
-                                //console.log(min);
-                                if ( time_fcts[data.time_fct].nodes.map(function(n) { return n.affiliate_fct_id; }).indexOf(affiliate.affiliate_fct_id) == -1) {
-                                    time_fcts[data.time_fct].nodes.push(affiliate);
-                                    //console.log('Added Node: ' + affiliate.value + ' in ' + data.time_fct);
-                                }
-                                var link = {
-                                    target_affiliate_fct_id : data.affiliate_fct_id, 
-                                    source_affiliate_fct_id : affiliate.affiliate_fct_id, 
-                                    value : affiliate.occurrence
-                                };
-                                time_fcts[data.time_fct].links.push(link);
-                                //console.log(time_fcts[data.time_fct]);
-                                //console.log('Added Link: ' + time_fcts[data.time_fct].nodes[link.source].affiliate_fct + ' > ' + time_fcts[data.time_fct].nodes[link.target].affiliate_fct);
-                            }
-
-                        }
-                    );
-                }
-
-                }
                 }
             );
-            }
         }
+
+        }
+        }
+    );
     );
     saveJSON(saveDir, 'detail-'+ minOccurrence +'.json', time_fcts);
 };

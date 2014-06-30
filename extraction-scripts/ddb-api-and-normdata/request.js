89c4b5c70aa4bc83d4980d42e19d27f3f8382bab
Gabriel Credico <me@gabrielcredico.de>
2014-06-30 12:07:38 +0200

    added node-sorting by occurrence, changed to bigger network... more bugs again :)

--- extraction-scripts/ddb-api-and-normdata/request.js
+++ extraction-scripts/ddb-api-and-normdata/request.js
@@ -1,12 +1,7 @@
 var request = require('request');
 
 var options = {
-    url: 'https://api.deutsche-digitale-bibliothek.de/search?facet=affiliate_fct&facet=place_fct&facet=affiliate_fct_normdata&rows=1&affiliate_fct=Borchert, Christian (Fotograf)',
-    headers: {
-        'Accept': 'application/json',
-        'Authorization':'OAuth oauth_consumer_key="vdiFptc5jO0P9DULDOZZ9R4le2UVcbSTNlEzuZ9YhZaCBMbukJv1403823224319"',
-        'Host':'api.deutsche-digitale-bibliothek.de'
-    }
+    url: 'http://lobid.org/person?name=Johann+Sebastian+Bach&format=full',
 };
 
 function callback(error, response, body) {
--- microsite/affiliates-network/js/DDBAffiliateNetwork.js
+++ microsite/affiliates-network/js/DDBAffiliateNetwork.js
@@ -3,10 +3,11 @@ DDBAffiliateNetwork = function()
 
   this.svgWidth = $(window).width();
 
-  this.timeFcts = ['time_61900', 'time_61947', 'time_61975', 'time_62000', 'time_62010', 'time_62020', 'time_62050', 'time_62100', 'time_62120', 'time_61925', 'time_62070', 'time_61800', 'time_61875', 'time_62110', 'time_62060', 'time_62080', 'time_62090', 'time_62095', 'time_61700', 'time_61705', 'time_61500', 'time_61545', 'time_61825', 'time_61600', 'time_61645', 'time_61847', 'time_62040', 'time_61745', 'time_62030', 'time_61907', 'time_61605', 'time_61807', 'time_61145', 'time_61100', 'time_61300', 'time_61305', 'time_61505', 'time_60500', 'time_60600', 'time_60700', 'time_60800', 'time_60900', 'time_61000', 'time_61105', 'time_61200', 'time_61205', 'time_61245', 'time_61345', 'time_61400', 'time_61405', 'time_61445', 'time_39600', 'time_39000', 'time_39700', 'time_60100', 'time_60200', 'time_60300', 'time_60400', 'time_39500', 'time_38900', 'time_38800', 'time_38000', 'time_39400', 'time_39300', 'time_38500', 'time_39800', 'time_38300', 'time_38100', 'time_39900', 'time_38400', 'time_38950', 'time_39200', 'time_37000', 'time_37700', 'time_38600', 'time_14000', 'time_39950', 'time_37400', 'time_36950', 'time_37500', 'time_37600', 'time_37900', 'time_37950', 'time_36000', 'time_38700', 'time_37800', 'time_38200', 'time_35000', 'time_36900', 'time_37100', 'time_17000', 'time_37200', 'time_39100', 'time_37300', 'time_34000', 'time_33000', 'time_15000', 'time_36800', 'time_36500', 'time_13000', 'time_36300', 'time_12000', 'time_18000', 'time_16000', 'time_30000', 'time_36200', 'time_36600', 'time_36700', 'time_36400', 'time_10000', 'time_60000', 'time_36100'];
-  this.timeNames = ['19. Jahrhundert', '1851 bis 1875', '1876 bis 1900', '20. Jahrhundert', '1901 bis 1910', '1911 bis 1920', '1941 bis 1950', '21. Jahrhundert', '2011 bis 2020', '1826 bis 1850', '1961 bis 1970', '18. Jahrhundert', '1776 bis 1800', '2001 bis 2010', '1951 bis 1960', '1971 bis 1980', '1981 bis 1990', '1991 bis 2000', '17. Jahrhundert', '1601 bis 1650', '15. Jahrhundert', '1451 bis 1500', '1726 bis 1750', '16. Jahrhundert', '1551 bis 1600', '1751 bis 1775', '1931 bis 1940', '1651 bis 1700', '1921 bis 1930', '1801 bis 1825', '1501 bis 1550', '1701 bis 1725', '1051 bis 1100', '11. Jahrhundert', '13. Jahrhundert', '1201 bis 1250', '1401 bis 1450', '5. Jahrhundert', '6. Jahrhundert', '7. Jahrhundert', '8. Jahrhundert', '9. Jahrhundert', '10. Jahrhundert', '1001 bis 1050', '12. Jahrhundert', '1101 bis 1150', '1151 bis 1200', '1251 bis 1300', '14. Jahrhundert', '1301 bis 1350', '1351 bis 1400', '5. Jahrhundert vor Christus', '1. Jahrtausend vor Christus', '4. Jahrhundert vor Christus', '1. Jahrhundert', '2. Jahrhundert', '3. Jahrhundert', '4. Jahrhundert', '6. Jahrhundert vor Christus', '12. Jahrhundert vor Christus', '13. Jahrhundert vor Christus', '2. Jahrtausend vor Christus', '7. Jahrhundert vor Christus', '8. Jahrhundert vor Christus', '16. Jahrhundert vor Christus', '3. Jahrhundert vor Christus', '18. Jahrhundert vor Christus', '20. Jahrhundert vor Christus', '2. Jahrhundert vor Christus', '17. Jahrhundert vor Christus', '11. Jahrhundert vor Christus', '9. Jahrhundert vor Christus', '3. Jahrtausend vor Christus', '24. Jahrhundert vor Christus', '15. Jahrhundert vor Christus', 'Trias', '1. Jahrhundert vor Christus', '27. Jahrhundert vor Christus', '31. Jahrhundert vor Christus', '26. Jahrhundert vor Christus', '25. Jahrhundert vor Christus', '22. Jahrhundert vor Christus', '21. Jahrhundert vor Christus', '4. Jahrtausend vor Christus', '14. Jahrhundert vor Christus', '23. Jahrhundert vor Christus', '19. Jahrhundert vor Christus', '5. Jahrtausend vor Christus', '32. Jahrhundert vor Christus', '30. Jahrhundert vor Christus', 'Tertiär', '29. Jahrhundert vor Christus', '10. Jahrhundert vor Christus', '28. Jahrhundert vor Christus', '6. Jahrtausend vor Christus', '7. Jahrtausend vor Christus', 'Jura', '33. Jahrhundert vor Christus', '36. Jahrhundert vor Christus', 'Paläozoikum', '38. Jahrhundert vor Christus', 'Präkambrium', 'Quartär', 'Kreide', 'Urgeschichtliche Zeiträume (vor Christus)', '39. Jahrhundert vor Christus', '35. Jahrhundert vor Christus', '34. Jahrhundert vor Christus', '37. Jahrhundert vor Christus', 'Geologische Zeiträume', 'Historische Zeiträume', '40. Jahrhundert vor Christus'];
+  this.timeFcts = ['time_61947', 'time_61975', 'time_62010', 'time_62020', 'time_62050', 'time_62120', 'time_61925', 'time_62070', 'time_61875', 'time_62110', 'time_62060', 'time_62080', 'time_62090', 'time_62095', 'time_61705', 'time_61545', 'time_61825', 'time_61645', 'time_61847', 'time_62040', 'time_61745', 'time_62030', 'time_61907', 'time_61605', 'time_61807', 'time_61145', 'time_61305', 'time_61505', 'time_60500', 'time_60600', 'time_60700', 'time_60800', 'time_60900', 'time_61000', 'time_61105', 'time_61205', 'time_61245', 'time_61345', 'time_61405', 'time_61445', 'time_39600', 'time_39000', 'time_39700', 'time_60100', 'time_60200', 'time_60300', 'time_60400', 'time_39500', 'time_38900', 'time_38800', 'time_38000', 'time_39400', 'time_39300', 'time_38500', 'time_39800', 'time_38300', 'time_38100', 'time_39900', 'time_38400', 'time_38950', 'time_39200', 'time_37000', 'time_37700', 'time_38600', 'time_14000', 'time_39950', 'time_37400', 'time_36950', 'time_37500', 'time_37600', 'time_37900', 'time_37950', 'time_36000', 'time_38700', 'time_37800', 'time_38200', 'time_35000', 'time_36900', 'time_37100', 'time_17000', 'time_37200', 'time_39100', 'time_37300', 'time_34000', 'time_33000', 'time_15000', 'time_36800', 'time_36500', 'time_13000', 'time_36300', 'time_12000', 'time_18000', 'time_16000', 'time_30000', 'time_36200', 'time_36600', 'time_36700', 'time_36400', 'time_10000', 'time_60000', 'time_36100'];
+  this.timeNames = ['1851 bis 1875', '1876 bis 1900', '1901 bis 1910', '1911 bis 1920', '1941 bis 1950', '2011 bis 2020', '1826 bis 1850', '1961 bis 1970', '1776 bis 1800', '2001 bis 2010', '1951 bis 1960', '1971 bis 1980', '1981 bis 1990', '1991 bis 2000', '1601 bis 1650', '1451 bis 1500', '1726 bis 1750', '1551 bis 1600', '1751 bis 1775', '1931 bis 1940', '1651 bis 1700', '1921 bis 1930', '1801 bis 1825', '1501 bis 1550', '1701 bis 1725', '1051 bis 1100', '1201 bis 1250', '1401 bis 1450', '5. Jahrhundert', '6. Jahrhundert', '7. Jahrhundert', '8. Jahrhundert', '9. Jahrhundert', '10. Jahrhundert', '1001 bis 1050', '1101 bis 1150', '1151 bis 1200', '1251 bis 1300', '1301 bis 1350', '1351 bis 1400', '5. Jahrhundert vor Christus', '1. Jahrtausend vor Christus', '4. Jahrhundert vor Christus', '1. Jahrhundert', '2. Jahrhundert', '3. Jahrhundert', '4. Jahrhundert', '6. Jahrhundert vor Christus', '12. Jahrhundert vor Christus', '13. Jahrhundert vor Christus', '2. Jahrtausend vor Christus', '7. Jahrhundert vor Christus', '8. Jahrhundert vor Christus', '16. Jahrhundert vor Christus', '3. Jahrhundert vor Christus', '18. Jahrhundert vor Christus', '20. Jahrhundert vor Christus', '2. Jahrhundert vor Christus', '17. Jahrhundert vor Christus', '11. Jahrhundert vor Christus', '9. Jahrhundert vor Christus', '3. Jahrtausend vor Christus', '24. Jahrhundert vor Christus', '15. Jahrhundert vor Christus', 'Trias', '1. Jahrhundert vor Christus', '27. Jahrhundert vor Christus', '31. Jahrhundert vor Christus', '26. Jahrhundert vor Christus', '25. Jahrhundert vor Christus', '22. Jahrhundert vor Christus', '21. Jahrhundert vor Christus', '4. Jahrtausend vor Christus', '14. Jahrhundert vor Christus', '23. Jahrhundert vor Christus', '19. Jahrhundert vor Christus', '5. Jahrtausend vor Christus', '32. Jahrhundert vor Christus', '30. Jahrhundert vor Christus', 'Tertiär', '29. Jahrhundert vor Christus', '10. Jahrhundert vor Christus', '28. Jahrhundert vor Christus', '6. Jahrtausend vor Christus', '7. Jahrtausend vor Christus', 'Jura', '33. Jahrhundert vor Christus', '36. Jahrhundert vor Christus', 'Paläozoikum', '38. Jahrhundert vor Christus', 'Präkambrium', 'Quartär', 'Kreide', 'Urgeschichtliche Zeiträume (vor Christus)', '39. Jahrhundert vor Christus', '35. Jahrhundert vor Christus', '34. Jahrhundert vor Christus', '37. Jahrhundert vor Christus', 'Geologische Zeiträume', 'Historische Zeiträume', '40. Jahrhundert vor Christus'];
   this.parameters = { timeFct : "", affiliateMinOccurrence : 40 };
 
+  this.nodePositioning = "network";
 
   this.networkWidth = $(window).width(), this.networkHeight = $(window).height();
 
@@ -70,6 +71,23 @@ DDBAffiliateNetwork = function()
         }
 
 
+        // calculate the sum of all occurrences
+        for (var i=0; i<keys.length; i++) {
+          var nodes = this.data[keys[i]].nodes;
+          nodes.forEach(function (n) {
+            if (self.nodeValues.has(n.affiliate_fct_id) == true) {
+              self.nodeValues.get(n.affiliate_fct_id).affiliate_fct_occurrence_sum+=n.affiliate_fct_occurrence;
+              self.nodeValues.get(n.affiliate_fct_id).affiliate_fct_occurrence_per_time_fct[n.affiliate_fct_id] = n.affiliate_fct_occurrence
+            } else {
+              self.nodeValues.set(n.affiliate_fct_id, {
+                affiliate_fct_occurrence_sum : n.affiliate_fct_occurrence,
+                affiliate_fct_occurrence_per_time_fct : {}
+              });
+            }
+          });
+        }
+
+
         // generate the new nodelist which will be compared
         for (var i=0; i<keys.length; i++) {
           var nodes = this.data[keys[i]].nodes;
@@ -85,10 +103,11 @@ DDBAffiliateNetwork = function()
           });
         }
 
+
+        // remove and add nodes to the current graph
+
         var keysToRemove = _.difference(this.currentNodes.keys(), newNodes.keys());
         var keysToAdd = _.difference(newNodes.keys(), this.currentNodes.keys());
-
-        //console.log(keysToAdd.length);
 
         keysToRemove.forEach(function (k) {
           self.currentNodes.remove(k);
@@ -99,6 +118,8 @@ DDBAffiliateNetwork = function()
         });
 
 
+        // generate current links
+        this.currentLinks = d3.map();
         for (var i=0; i<keys.length; i++) {
           var links = this.data[keys[i]].links;
           links.forEach(function (l) {
@@ -111,106 +132,24 @@ DDBAffiliateNetwork = function()
           });
         }
 
+        if (this.nodePositioning == "network") {
 
         this.force
             .nodes( this.currentNodes.values() )
-            .links( this.currentLinks.values() )
-            .start();
+            .links( this.currentLinks.values() );
+          this.force.start();
+        }
 
-        this.force.start();
         this.load();
-        console.log('change');
-
-        // calculate the sum of all occurrences
-        this.currentLinks = d3.map();
 
       } else {
-        console.log('no change');
       }
     };
-
-
-/*
-
-    this.updateData = function () {
-
-      //console.log(this.data[this.parameters.timeFct].nodes.length);
-
-      var addCount=0, existingCount=0, deleteCount=0;
-
-/*
-      for (var i=0; i < this.currentNodes.length; i++)
-      {
-        // delete nodes that arent in the new array
-         if ( this.data[this.parameters.timeFct].nodes.map( function (n) { return n.affiliate_fct_id; }).indexOf(this.currentNodes[i].affiliate_fct_id) == -1 ) {
-          //console.log( this.currentNodes[i] );
-          this.currentNodes.splice(this.currentNodes.indexOf(i, 1);
-          deleteCount++;
-        } else {
-          existingCount++;
-        }
-      }
-      console.log('Deleted obsolete nodes: ' + deleteCount+' | Kept existing nodes: ' + existingCount);
-
-      var addCount=0, existingCount=0;
-
-    
-      // console.log(this.currentNodes.length);
-    
-      // var t = _.intersection(this.currentNodes, this.data[this.parameters.timeFct].nodes);
-
-      // t.forEach(function (n) {
-      //   n.fixed = true;
-      //   n.y = $(window).height()/2;
-      // });
-
-      // this.currentNodes = t;
-
-      // console.log(this.currentNodes.length);
-
-      this.currentNodes = this.force.nodes();
-      var obsoleteNodes = _.difference(this.currentNodes, this.data[this.parameters.timeFct].nodes);
-
-      //console.log(obsoleteNodes);
-
-      self=this;
-
-      if (obsoleteNodes.length > 0 && this.currentNodes.length > 0) {
-        obsoleteNodes.forEach( function (el, i) {
-          var index = self.currentNodes.map(function (n2) { return n2.affiliate_fct_id; }).indexOf(el.affiliate_fct_id);
-          self.currentNodes.splice(index, 1 );
-        });
-      }
-
-
-      for (var i=0; i<this.data[this.parameters.timeFct].nodes.length; i++)
-      {
-        if ( this.currentNodes.map( function (n) { return n.affiliate_fct_id; }).indexOf(this.data[this.parameters.timeFct].nodes[i].affiliate_fct_id) == -1 ) {
-          this.currentNodes.push(this.data[this.parameters.timeFct].nodes[i]);
-          addCount++;
-        } else {
-          existingCount++;
-        }
-      }
-
-      console.log('Added new nodes: '+addCount+' | Skipped existing nodes: ' + existingCount);
-
-
-    // self.force
-    //     .nodes( this.currentNodes )
-    //     .links( this.currentLinks );
-
-
-    };
-
-    */
 
 
   this.initTimeline = function () {
 
     this.timelineWidth = 940;
-
-
     var brushScale = d3.scale.linear()
       .domain([0,this.timeFcts.length-1])
       .range([0, this.timelineWidth]);
@@ -263,7 +202,7 @@ DDBAffiliateNetwork = function()
       .append('rect')
       .attr('x', function (d, i) { return brushScale(i); } )
       .attr('y', 0 )
-      .attr('width', function (d, i) { return brushScale(1); })
+      .attr('width', function (d, i) { return brushScale(1)-1; })
       .attr('height', 0 )
       .attr('fill', '#a40539')
       .attr('fill-opacity', 1)
@@ -296,6 +235,89 @@ DDBAffiliateNetwork = function()
 
   };
 
+  this.switchNodepositioning = function (mode) {
+
+    switch(mode) {
+      case "network" :
+
+        this.nodePositioning = "network";
+        this.affiliates
+          .transition()
+          .duration(1000)
+          .attr("cx", function (d) { return self.nodeValues.get(d.affiliate_fct_id).networkPosition.x; })
+          .attr("cy", function (d) { return self.nodeValues.get(d.affiliate_fct_id).networkPosition.y; })
+          .attr("r", function (d) { return Math.sqrt( (self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum) / Math.PI); })
+          .call(function (d,i) { if (i=d.length-1) { self.force.start();} });
+        this.updateNodes();
+
+      break;
+      case "sortByOccurrence" :
+
+        this.nodePositioning = "sortByOccurrence";
+        this.force.stop();
+
+        this.links.remove();
+
+        // copies the network-positions
+        this.nodeValues.keys().forEach(function (id) {
+          self.nodeValues.get(id).networkPosition = {
+            x : self.currentNodes.get(id).x,
+            y : self.currentNodes.get(id).y
+          };
+        });
+      this.updateNodes();
+      break;
+
+    }
+  };
+
+
+  this.updateNodes = function () {
+
+    switch(this.nodePositioning) {
+
+      case "network" :
+        this.affiliates
+          .transition()
+          .duration(1000)
+          .attr("cx", function (d) { return d.x; })
+          .attr("cy", function (d) { return d.y; })
+          .attr("r", function (d) { return Math.sqrt( (self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum) / Math.PI); });
+
+
+      break;
+
+      case "sortByOccurrence" :
+        var maxOccurence = d3.max(this.nodeValues.values(), function (nv) {
+          return nv.affiliate_fct_occurrence_sum;
+        });
+
+        var minOccurence = d3.min(this.nodeValues.values(), function (nv) {
+          return nv.affiliate_fct_occurrence_sum;
+        });
+
+        var sortScale = d3.scale.log(2).domain([minOccurence, maxOccurence]).range([200, this.networkWidth-200]);
+
+        this.affiliates
+          .call( function (d,i) { d.fixed = true;})
+          .transition()
+          .duration(500)
+          .attr("r", function (d) { return Math.sqrt( (self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum) / Math.PI); })
+          .attr("cx", function (d,i) { return sortScale(self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum); })
+          .attr("cy", function (d,i) { return self.networkHeight/2 } );
+      break;
+
+    }
+
+  }
+
+
+  this.loadNormdata = function (affiliateFacetID) {
+
+      jQuery.getJSON("http://api.lobid.org/person?name="+this.allNodes.get(affiliateFacetID).affiliate_fct+"&format=full" , function (result) {
+        console.log(result);
+      });
+  };
 
 
 
@@ -321,7 +343,7 @@ DDBAffiliateNetwork = function()
     this.currentLinks = d3.map();
 
     /* Initialize tooltip */
-    this.tip = d3.tip().attr('class', 'd3-tip').html( function (d) { return '<strong>'+d.affiliate_fct+'</strong> ('+d.affiliate_fct_occurrence+' Einträge)'; } );
+    this.tip = d3.tip().attr('class', 'd3-tip').html( function (d) { return '<h3>'+d.affiliate_fct+'</h3><p>'+self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum+' Einträge)</p>'; } );
     this.svg.call(this.tip);     /* Invoke the tip in the context of your visualization */
 
     this.parameters.timeFct = this.timeFcts[0];
@@ -398,7 +420,6 @@ DDBAffiliateNetwork = function()
 
   this.load = function () {
 
-
     this.links = this.svg
       .selectAll(".link")
       .data( this.force.links() );
@@ -428,12 +449,7 @@ DDBAffiliateNetwork = function()
       .data( this.force.nodes(), function (d) { return "affiliate_fct_id-" + d.affiliate_fct_id; } );
 
 
-    // update existing
-    this.affiliates
-      .transition()
-      .duration(1000)
-      .attr("r", function (d) { return Math.sqrt( (self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum) / Math.PI); });
-      //.style("fill", '#000');
+      this.updateNodes();
 
     // enter new affiliates
     this.affiliates
@@ -447,6 +463,7 @@ DDBAffiliateNetwork = function()
       .attr("class", "affiliate")
       .style("fill", '#cecece')
       .call( this.force.drag )
+      .on("click", function (d) { self.loadNormdata(d.affiliate_fct_id); })
       .transition()
       .duration(500)
       //.delay( function (d, i) { return i*3; } )
@@ -488,32 +505,6 @@ DDBAffiliateNetwork = function()
           .call( function (d, i) { d.fixed = false; if (i==d.length-1) {self.force.start();} } )
           .attr("cx", function (d) { return d.px; })
           .attr("cy", function (d) { return d.py; });
-    }
-
-
-    this.sortByOccurence = function () {
-      // d3.select(".fixed-affiliate")
-      //   .style("fill", "#cecece")
-      //   .call( function (d) {d.fixed = false; self.force.start();} );
-
-      var maxOccurence = d3.max(this.nodeValues.values(), function (nv) {
-        return nv.affiliate_fct_occurrence_sum;
-      });
-
-      var minOccurence = d3.min(this.nodeValues.values(), function (nv) {
-        return nv.affiliate_fct_occurrence_sum;
-      });
-
-      var sortScale = d3.scale.log(2).domain([minOccurence, maxOccurence]).range([200, this.networkWidth-200]);
-
-      this.affiliates
-        .call( function (d,i) {if(i==0){self.force.stop(); d.fixed = true;}})
-        .transition()
-        .duration(500)
-        .attr("cx", function (d,i) { return sortScale(self.nodeValues.get(d.affiliate_fct_id).affiliate_fct_occurrence_sum); })
-        .attr("cy", function (d,i) { return i.networkHeight/2 } );
-        //console.log(d);
-
     }
 
 
